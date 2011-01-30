367df85d77baf1e910a7e393f4872ed5d41b9bbe
alexbowe <bowe.alexander@gmail.com>
2011-01-31 02:54:17 +1100

    add template support

--- gencv.py
+++ gencv.py
@@ -1,9 +1,17 @@
 data_file = 'data.yaml'
+template_file = 'template.md'
 
 import yaml
+from Cheetah.Template import Template
 
 # Load YAML into data dictionary
 with open(data_file) as f:
-	data = yaml.load(f)
+    data = yaml.load(f)
 
-print data['summary']
+# Load template
+with open(template_file) as f:
+    template = f.read()
+
+t = Template(template, searchList=[data])
+
+print t
