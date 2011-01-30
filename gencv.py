584be2c3aa31e476c89dfcdd4720c4074fa32b9d
alexbowe <bowe.alexander@gmail.com>
2011-01-31 04:33:22 +1100

    refactor into functions.
    This may be used for any YAML to Cheetah work, which
    I think will be quite common for my future projects.

--- gencv.py
+++ gencv.py
@@ -1,17 +1,19 @@
-data_file = 'data.yaml'
-template_file = 'template.md'
+default_data_file = 'data.yaml'
+default_template_file = 'template.md'
 
 import yaml
 from Cheetah.Template import Template
 
-# Load YAML into data dictionary
-with open(data_file) as f:
-    data = yaml.load(f)
+def yaml2cheetah(data_file, template_file):
+    # Load YAML into data dictionary
+    with open(data_file) as f:
+        data = yaml.load(f)
+	
+    # Load template
+    with open(template_file) as f:
+        template = f.read()
+	
+    return Template(template, searchList=[data])
 
-# Load template
-with open(template_file) as f:
-    template = f.read()
-
-t = Template(template, searchList=[data])
-
-print t
+if __name__ == '__main__':
+	print yaml2cheetah(default_data_file, default_template_file)
