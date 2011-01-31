b82a32e4216434a5dc51b90d1f603973bf9126ba
alexbowe <bowe.alexander@gmail.com>
2011-01-31 13:33:45 +1100

    add commandline arguments

--- gencv.py
+++ gencv.py
@@ -1,6 +1,3 @@
-default_data_file = 'data.yaml'
-default_template_file = 'template.md'
-
 import yaml
 from Cheetah.Template import Template
 
@@ -8,12 +5,32 @@ def yaml2cheetah(data_file, template_file):
     # Load YAML into data dictionary
     with open(data_file) as f:
         data = yaml.load(f)
-	
+
     # Load template
     with open(template_file) as f:
         template = f.read()
-	
+
     return Template(template, searchList=[data])
 
 if __name__ == '__main__':
-	print yaml2cheetah(default_data_file, default_template_file)
+    from optparse import OptionParser
+
+    parser = OptionParser()
+    parser.add_option("-d", "--data", dest="data_file",
+		      help="YAML file containing data",
+		      metavar="FILE")
+    parser.add_option("-t", "--template", dest="template_file",
+		      help="Template for output",
+		      metavar="FILE")
+    mandatory = ['data_file', 'template_file']
+
+    options, args = parser.parse_args()
+    
+    for m in mandatory:
+        if not options.__dict__[m]:
+	    parser.print_help()
+            parser.error("Mandatory option is missing")
+    
+    data_file = options.data_file
+    template_file = options.template_file
+    print yaml2cheetah(data_file, template_file)
